02ce2a79e1a9abe10afd3d79a110657e2a09c995
Gaston De los Reyes <mu_gas@hotmail.com>
2020-01-14 00:27:44 -0300

    Credenciales

--- 07 - Rest Server/server/config/config.js
+++ 07 - Rest Server/server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:gkc6cXawapZG5Gzv@cluster0-w3cpw.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB
